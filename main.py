e716c5dab17a6ef3223b978ac5d8cfa4c7325bb9
Isaac-A-UofT <isaac.abell@mail.utoronto.ca>
2025-01-28 15:55:05 -0500

    formatting changes

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ from botocore.exceptions import BotoCoreError, ClientError
 from api_tutorials_generator.api_helper import module, curriculum, curriculum_template
 
 REGION = "us-east-2"
+IDENTITY="Professor of Computer Science"
+TARGET_AUDIENCE="first year computer science students"
+GENERATE_WIKI=False
+MODEL="gpt-4o"
 
 # Initialize the S3 client
 s3_client = s3_client = boto3.client("s3")
@@ -18,32 +22,32 @@ app = FastAPI()
 
 class CurriculumRequest(BaseModel):
     topic: str
-    identity: Optional[str] = "Professor of Computer Science"
-    target_audience: Optional[str] = "first year computer science students"
-    generate_wiki: bool = False
+    identity: Optional[str] = IDENTITY
+    target_audience: Optional[str] = TARGET_AUDIENCE
+    generate_wiki: bool = GENERATE_WIKI
 
 class ModuleRequest(BaseModel):
     topic: str
-    identity: Optional[str] = "Professor of Computer Science"
-    target_audience: Optional[str] = "first year computer science students"
-    generate_wiki: bool = False
+    identity: Optional[str] = IDENTITY
+    target_audience: Optional[str] = TARGET_AUDIENCE
+    generate_wiki: bool = GENERATE_WIKI
 
 
 # Define the asynchronous functions
-async def generate_curriculum(topic, identity, target_audience, generate_wiki = False):
+async def generate_curriculum(topic, identity, target_audience, generate_wiki = GENERATE_WIKI):
     try:
 
-        template = json.loads(await curriculum_template(topic, identity, target_audience, "gpt-4o"))
-        files_data = await curriculum(identity, target_audience, template, "gpt-4o", template, generate_wiki)
+        template = json.loads(await curriculum_template(topic, identity, target_audience, MODEL))
+        files_data = await curriculum(identity, target_audience, MODEL, template, generate_wiki)
 
         return {"status": 200, **files_data}
     except Exception as e:
         print(f"Error in generate_curriculum: {e}")
         return {"status": 500}
 
-async def generate_module(topic, identity="Professor of Computer Science", target_audience="first year computer science students", generate_wiki = False):
+async def generate_module(topic, identity=IDENTITY, target_audience=TARGET_AUDIENCE, generate_wiki = GENERATE_WIKI):
     try:
-        files_data = await module(topic, identity, target_audience, "gpt-4o", generate_wiki)
+        files_data = await module(topic, identity, target_audience, MODEL, generate_wiki)
         return {"status": 200, **files_data}
     except Exception as e:
         print(f"Error in generate_module: {e}")
@@ -67,10 +71,10 @@ async def create_module(request: ModuleRequest):
         request.topic,
         request.identity,
         request.target_audience,
-        request.generate_wiki or False
+        request.generate_wiki or GENERATE_WIKI
     )
     if result['status'] != 200:
-        raise HTTPException(status_code=result['status'], detail=f"Failed generate file")    
+        raise HTTPException(status_code=result['status'], detail=f"Failed to generate file")    
     try:
         # Define the bucket name and file name with UUID
         bucket_name = "joltedmod"
@@ -96,5 +100,5 @@ async def create_module(request: ModuleRequest):
         file_url = f"https://{bucket_name}.s3.{REGION}.amazonaws.com/{file_name}"
         return {"status": 200, "url": file_url}
 
-    except (BotoCoreError, ClientError) as e:
-        raise HTTPException(status_code=500, detail=f"Failed send file")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Failed to send file")
